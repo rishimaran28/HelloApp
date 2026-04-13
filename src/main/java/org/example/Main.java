41d2606bdedb48114458f7a339dfa2abbc9ab6d2
rishimaran28 <rishi.maran28@gmail.com>
2026-04-13 10:55:38 +0530

    Coded UC5:Display Hello World in console

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,16 +1,11 @@
-public class UC4Hello {
-
+public class Main {
     public static void main(String[] args) {
-        if (args.length > 0) {
-            // Print "Hello" + all command-line arguments
-            System.out.print("Hello");
-            for (String arg : args) {
-                System.out.print(" " + arg);
-            }
-            System.out.println();
+        if (args.length == 0) {
+            System.out.println("Hello");
         } else {
-            // Default message when no arguments are passed
-            System.out.println("Hello, World!");
+            for (String arg : args) {
+                System.out.println("Hello " + arg);
+            }
         }
     }
 }
