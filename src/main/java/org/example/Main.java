7ea6f054a2595b9ded5f7d3e11a0705eaa6bf847
rishimaran28 <rishi.maran28@gmail.com>
2026-04-13 10:28:30 +0530

    Coded UC1: Display Hello World in console

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,10 +1,12 @@
-package org.example;
+
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    static void main() {
-        System.out.println("HelloWorld!");
+    static void main(String[] args) {
+        String name = args[0];
+        System.out.println("Hello, " + name + "!");
+
 
     }
 }
