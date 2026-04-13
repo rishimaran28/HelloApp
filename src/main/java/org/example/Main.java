a41334f1626853075f46166f84f74c98cc989168
rishimaran28 <rishi.maran28@gmail.com>
2026-04-13 10:40:52 +0530

    Coded UC3: Display Hello World in console

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,12 +1,19 @@
+public class UC3 {
+    public static void main(String[] args) {
 
+        if (args.length > 0) {
+            // Option A: use only the first word
+            System.out.println(args[0]);
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    static void main(String[] args) {
-        String name = args[0];
-        System.out.println("Hello, " + name + "!");
-
-
+            // Option B (if you want full message):
+            // StringBuilder sb = new StringBuilder();
+            // for (int i = 0; i < args.length; i++) {
+            //     if (i > 0) sb.append(" ");
+            //     sb.append(args[i]);
+            // }
+            // System.out.println(sb.toString());
+        } else {
+            System.out.println("Hello World"); // default message
+        }
     }
 }
